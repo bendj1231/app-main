9e721527401d5bf33e8b4fb2f3f13644f9a0df52
bendj1231 <wingmentorprogram@gmail.com>
2026-04-20 20:58:50 +0800

    Upload private charter image to Cloudinary and update pathways page

--- components/website/components/pathways/PrivateCharterPathwaysPage.tsx
+++ components/website/components/pathways/PrivateCharterPathwaysPage.tsx
@@ -57,7 +57,7 @@ export const PrivateCharterPathwaysPage: React.FC<PrivateCharterPathwaysPageProp
                         <div className="relative w-full mx-auto">
                             <div className="aspect-video bg-slate-100 rounded-3xl overflow-hidden shadow-lg relative group">
                                 <img
-                                    src="https://lh3.googleusercontent.com/d/1zefsTeyIZBEeCjhgHhbgjtk5x8rvZEwU"
+                                    src="https://res.cloudinary.com/dridtecu6/image/upload/v1776689902/f_auto,q_auto,w_1200/pathways/private-charter.jpg"
                                     alt="Private Charter Aircraft"
                                     className="w-full h-full object-cover transition-transform duration-700 group-hover:scale-105"
                                 />
