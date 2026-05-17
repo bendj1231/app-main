ef7d50c1518ef8ed2902305a44d171946db16e4a
bendj1231 <wingmentorprogram@gmail.com>
2026-05-17 23:06:56 +0800

    fix(home): shift AccessPlatformCard photo focus to left with object-left

--- components/website/components/home/PathwayGrid.tsx
+++ components/website/components/home/PathwayGrid.tsx
@@ -521,7 +521,7 @@ const AccessPlatformCard: React.FC<{
                 <img
                     src="https://res.cloudinary.com/dridtecu6/image/upload/v1776948158/sedmmczhyibdw1okfcgx.png"
                     alt="Pilots"
-                    className="w-full h-full object-cover object-center"
+                    className="w-full h-full object-cover object-left"
                 />
                 <div className="absolute inset-0 bg-gradient-to-l from-transparent to-[#0a1628]/60 pointer-events-none" />
             </div>
